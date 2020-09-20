c458fb4431bc6ffd4cf7f24bf23c6772d82753cb
astrimuller <astri.muller@gmail.com>
2020-09-20 00:25:41 -0400

    Login

--- app/src/main/java/com/example/appdengue/Login.java
+++ app/src/main/java/com/example/appdengue/Login.java
@@ -1,13 +1,26 @@
 package com.example.appdengue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+    TextView tv_registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tv_registrar= (TextView) findViewById(R.id.tv_registrar);
+
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentReg = new Intent(Login.this, Registro.class);
+                Login.this.startActivity(intentReg);
+            }
+        });
+        }
     }
-}
--- app/src/main/java/com/example/appdengue/MainActivity.java
+++ app/src/main/java/com/example/appdengue/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appdengue;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -18,6 +19,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 Button btnGPS;
 TextView tvUbicacion;
+Button acceder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +58,16 @@ TextView tvUbicacion;
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         1);
             }
+
         }
+            acceder= (Button) findViewById(R.id.button_acceder);
+
+            acceder.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intentAcceder = new Intent(MainActivity.this, Login.class);
+                    MainActivity.this.startActivity(intentAcceder);
+                }
+            });
     }
 }
