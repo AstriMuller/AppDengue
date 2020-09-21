c35420b8d8bc317c58c3b037a6fb526c390edb55
astrimuller <astri.muller@gmail.com>
2020-09-21 01:24:49 -0400

    configuraciones bd

--- app/src/main/java/com/example/appdengue/Login.java
+++ app/src/main/java/com/example/appdengue/Login.java
@@ -1,19 +1,44 @@
 package com.example.appdengue;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Login extends AppCompatActivity {
+    EditText edt_usuario, edt_contrasenha;
     TextView tv_registrar;
+    Button bt_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edt_usuario = findViewById(R.id.edt_usuario);
+        edt_contrasenha = findViewById(R.id.edt_contrasenha);
 
-        tv_registrar= (TextView) findViewById(R.id.tv_registrar);
+        tv_registrar = (TextView) findViewById(R.id.tv_registrar);
+        bt_login = (Button) findViewById(R.id.bt_ingresar);
 
         tv_registrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +47,40 @@ public class Login extends AppCompatActivity {
                 Login.this.startActivity(intentReg);
             }
         });
-        }
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String user_usuario=edt_usuario.getText().toString();
+                final String user_contrasenha=edt_contrasenha.getText().toString();
+
+                Response.Listener<String> responseListener=new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new  JSONObject(response);
+                            boolean success= jsonResponse.getBoolean("success");
+                            if (success){
+                                Intent intent_inicio = new Intent(Login.this,Inicio.class);
+                                Login.this.startActivity(intent_inicio);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                                builder.setMessage("error login")
+                                        .setNegativeButton("Retry",null)
+                                        .create().show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                LoginRequest loginRequest= new LoginRequest(user_usuario, user_contrasenha, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Login.this);
+                queue.add(loginRequest);
+            }
+        });
     }
+
+
+}
--- app/src/main/java/com/example/appdengue/Registro.java
+++ app/src/main/java/com/example/appdengue/Registro.java
@@ -1,13 +1,66 @@
 package com.example.appdengue;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Registro extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+EditText edt_usuario, edt_email, edt_contrasenha;
+Button bt_registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        edt_usuario=(EditText) findViewById(R.id.edit_usuario);
+        edt_email=(EditText) findViewById(R.id.edit_email);
+        edt_contrasenha=(EditText) findViewById(R.id.edit_contrasenha);
+
+        bt_registro= (Button) findViewById(R.id.btn_registrar);
+        bt_registro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final String user_usuario=edt_usuario.getText().toString();
+        final String user_email=edt_email.getText().toString();
+        final String user_contrasenha=edt_contrasenha.getText().toString();
+        Response.Listener<String> respoListener =new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonReponse = new JSONObject(response);
+                    boolean success= jsonReponse.getBoolean("success");
+                    if (success){
+                        Intent intent = new Intent(Registro.this,Login.class);
+                        Registro.this.startActivity(intent);
+
+                    }else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+                        builder.setMessage("error registro")
+                                .setNegativeButton("Retry",null)
+                                .create().show();
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        RegistroRequest registroRequest= new RegistroRequest(user_usuario, user_email, user_contrasenha, respoListener);
+        RequestQueue queue = Volley.newRequestQueue(Registro.this);
+        queue.add(registroRequest);
     }
 }
