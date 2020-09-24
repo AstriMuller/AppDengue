aec3d704fe2f0a9f7bbc9ff02503b17ef97da4cc
astrimuller <astri.muller@gmail.com>
2020-09-24 16:26:44 -0400

    Validación

--- app/src/main/java/com/example/appdengue/Registro.java
+++ app/src/main/java/com/example/appdengue/Registro.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -26,41 +27,56 @@ Button bt_registro;
         edt_usuario=(EditText) findViewById(R.id.edit_usuario);
         edt_email=(EditText) findViewById(R.id.edit_email);
         edt_contrasenha=(EditText) findViewById(R.id.edit_contrasenha);
-
         bt_registro= (Button) findViewById(R.id.btn_registrar);
         bt_registro.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        final String user_usuario=edt_usuario.getText().toString();
-        final String user_email=edt_email.getText().toString();
-        final String user_contrasenha=edt_contrasenha.getText().toString();
-        Response.Listener<String> respoListener =new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonReponse = new JSONObject(response);
-                    boolean success= jsonReponse.getBoolean("success");
-                    if (success){
-                        Intent intent = new Intent(Registro.this,Login.class);
-                        Registro.this.startActivity(intent);
+        /// validar
+        if (edt_usuario.getText().toString().isEmpty()){
+            Toast.makeText(this,"Campo usuario vacío",Toast.LENGTH_LONG).show();
+        }else {
+           if (edt_email.getText().toString().isEmpty()){
+               Toast.makeText(this,"Campo email vacío",Toast.LENGTH_LONG).show();
+           }else {
+               if (edt_contrasenha.getText().toString().isEmpty()){
+                   Toast.makeText(this,"Campo contraseña vacío",Toast.LENGTH_LONG).show();
+               }else {
+                   ////
+                   final String user_usuario=edt_usuario.getText().toString();
+                   final String user_email=edt_email.getText().toString();
+                   final String user_contrasenha=edt_contrasenha.getText().toString();
+                   Response.Listener<String> respoListener =new Response.Listener<String>() {
+                       @Override
+                       public void onResponse(String response) {
+                           try {
+                               JSONObject jsonReponse = new JSONObject(response);
+                               boolean success= jsonReponse.getBoolean("success");
+                               if (success){
 
-                    }else{
-                        AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
-                        builder.setMessage("error registro")
-                                .setNegativeButton("Retry",null)
-                                .create().show();
+                                   Intent intent = new Intent(Registro.this,Login.class);
+                                   Registro.this.startActivity(intent);
 
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+                               }else{
+                                   AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+                                   builder.setMessage("error registro")
+                                           .setNegativeButton("Retry",null)
+                                           .create().show();
 
-        RegistroRequest registroRequest= new RegistroRequest(user_usuario, user_email, user_contrasenha, respoListener);
-        RequestQueue queue = Volley.newRequestQueue(Registro.this);
-        queue.add(registroRequest);
+                               }
+                           } catch (JSONException e) {
+                               e.printStackTrace();
+                           }
+                       }
+                   };
+
+                   RegistroRequest registroRequest= new RegistroRequest(user_usuario, user_email, user_contrasenha, respoListener);
+                   RequestQueue queue = Volley.newRequestQueue(Registro.this);
+                   queue.add(registroRequest);
+               }
+           }
+        }
+
     }
 }
