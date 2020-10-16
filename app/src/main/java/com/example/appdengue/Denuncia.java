acdbb32def7b17c7548c90aba5583640cef4cde5
astrimuller <astri.muller@gmail.com>
2020-10-16 03:11:13 -0300

    acceso a la camara

--- app/src/main/java/com/example/appdengue/Denuncia.java
+++ app/src/main/java/com/example/appdengue/Denuncia.java
@@ -2,32 +2,101 @@ package com.example.appdengue;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.File;
+import java.time.temporal.Temporal;
+
 public class Denuncia extends AppCompatActivity {
     Spinner sp_denuncia;
-    Button btnGPS;
-    TextView tvUbicacion;
+    Button btn_gps;
+    Button btn_camara;
+    ImageView iv_camara;
+
+
+    private final int cod_foto = 100;
+    private final int selec_foto = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_denuncia);
         sp_denuncia = (Spinner) findViewById(R.id.sp_denuncia);
-        btnGPS = (Button)findViewById(R.id.button);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.lista, android.R.layout.simple_list_item_1);
+        btn_gps = (Button) findViewById(R.id.button);
+        btn_camara = (Button) findViewById(R.id.btn_camara);
+        iv_camara = (ImageView) findViewById(R.id.iv_camara);
+
+        btn_camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final CharSequence[] options = {"Tomar foto", "Elegir de galeria", "Cancelar"};
+                final AlertDialog.Builder builder = new AlertDialog.Builder(Denuncia.this);
+                builder.setTitle("Elige una opción");
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int seleccion) {
+                        if (options[seleccion] == "Tomar foto") {
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(intent, cod_foto);
+                        } else if (options[seleccion] == "Elegir de galeria") {
+                            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            intent.setType("image/*");
+                            startActivityForResult(intent.createChooser(intent, "Seleciona una aplicación"), selec_foto);
+                        } else if (options[seleccion] == "Cancelar") {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                builder.show();
+            }
+
+        });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.lista, android.R.layout.simple_list_item_1);
         sp_denuncia.setAdapter(adapter);
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case cod_foto:
+                if (resultCode == RESULT_OK) {
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    iv_camara.setImageBitmap(bitmap);
+                }
+                break;
+            case selec_foto:
+                if (resultCode == RESULT_OK) {
+                    Uri path = data.getData();
+                    iv_camara.setImageURI(path);
+                }
+                break;
+        }
+    }
+
 }
