e818adaee1ce1f7cb714feb7c971803129aa3d94
astrimuller <astri.muller@gmail.com>
2020-12-07 17:26:22 -0300

    Configuraciones de vistas2

--- app/src/main/java/com/example/appdengue/MainActivity.java
+++ app/src/main/java/com/example/appdengue/MainActivity.java
@@ -1,37 +1,33 @@
 package com.example.appdengue;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    //Button btnGPS;
-//TextView tvUbicacion;
-    Button acceder;
+    Button btn_acceder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_acceder = (Button) findViewById(R.id.btn_acceder);
 
-        //acceder= (Button) findViewById(R.id.button_acceder);
-
-        acceder.setOnClickListener(new View.OnClickListener() {
+        btn_acceder.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intentAcceder = new Intent(MainActivity.this, Login.class);
-                MainActivity.this.startActivity(intentAcceder);
+            public void onClick(View v) {
+                Intent intentReg = new Intent(MainActivity.this, Tabbed.class);
+                MainActivity.this.startActivity(intentReg);
             }
         });
+        }
+
+
+
+
     }
-}
